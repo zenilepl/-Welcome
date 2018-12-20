6834ba99c2b5a74bab805df090a41c23fb5ddd04
zenilepl <45181384+zenilepl@users.noreply.github.com>
2018-12-20 14:40:18 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,69 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('guildMemberRemove', member => {
+    var embed = new Discord.RichEmbed()
+.setAuthor(member.user.username, member.user.avatarURL)
+.setThumbnail(member.user.avatarURL)
+.setTitle('***خرج من السيرفر***')
+.addField('**الاسم**',`[ ${member} ]`)
+.addField('**عدد الاعضاء**',`[ ${member.guild.memberCount} ]`,true)
+.setColor('Random')
+   
+   var channel =member.guild.channels.find('name', 'hub')
+   if (!channel) return;
+         channel.send({embed : embed});
+ 
+});
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'hub');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+    let embed = new Discord.RichEmbed()
+        .setColor('PURPLE')
+        .setThumbnail(memberavatar)
+        .addField(' | name :  ',`${member}`)
+        .addField(' | اطلق من دخل' , `Welcome to the server, ${member}`)
+        .addField(' | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                     
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter(`${member.guild.name}`)
+        .setTimestamp()
+   
+      channel.sendEmbed(embed);
+    });
+   
+    client.on('guildMemberRemove', member => {
+        var embed = new Discord.RichEmbed()
+        .setAuthor(member.user.username, member.user.avatarURL)
+        .setThumbnail(member.user.avatarURL)
+        .setTitle(`بس بعرف وين رحت؟؟؟ :raised_hand::skin-tone-1: :pensive:`)
+        .setDescription(`مع السلامه تشرفنا بك :raised_hand::skin-tone-1: :pensive: `)
+        .addField('   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+        .setColor('PURPLE')
+        .setFooter(`====اهلا السيرفر نور بيك و الله====`, 'https://cdn.discordapp.com/attachments/397818254439219217/399292026782351381/shy.png')
+   
+    var channel =member.guild.channels.find('name', 'hub')
+    if (!channel) return;
+    channel.send({embed : embed});
+ 
+    });
+	
+	client.on("guildMemberAdd", member => {
+    member.createDM().then(function (channel) {
+        return channel.send(`:rose:  ولكم نورت السيرفر :rose:
+        :crown: اسم العضو  ${member}:crown:  
+        انت العضو رقم ${member.guild.memberCount} `)
+    }).catch(console.error)
+})
+
+client.login('NTI1Mjg0MjIxMjkyNTExMjM0.Dv0cug.uDC-YBmslGvmkf28suJmNsfM6I4');
